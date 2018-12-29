84f1fcc5153f4ea86864b86d12d2f7cbffbc9039
KO-YAMAKO <ko.foot1103@gmail.com>
2018-12-29 15:31:45 +0900

    update xcode project and cocoapods

---
+++ scorpio/scorpio/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  scorpio
+//
+//  Created by 山浦功 on 2018/12/29.
+//  Copyright © 2018 com.yamaura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
