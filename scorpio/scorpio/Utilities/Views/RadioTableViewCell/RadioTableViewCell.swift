ab58db2a1dbd112517b2cf5e7919fe5451b0ddd4
KO-YAMAKO <ko.foot1103@gmail.com>
2018-12-29 20:20:27 +0900

    add RadioTableViewCell

---
+++ scorpio/scorpio/Utilities/Views/RadioTableViewCell/RadioTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RadioTableViewCell.swift
+//  scorpio
+//
+//  Created by 山浦功 on 2018/12/29.
+//  Copyright © 2018 com.yamaura. All rights reserved.
+//
+
+import UIKit
+
+class RadioTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
